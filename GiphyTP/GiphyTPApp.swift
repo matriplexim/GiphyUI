c6848271c0814e497eaa9044608bc16dcea81eca
matriplexim <matriplexim@users.noreply.github.com>
2023-01-28 14:53:38 +0400

    Initial Commit

---
+++ GiphyTP/GiphyTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyTPApp.swift
+//  GiphyTP
+//
+//  Created by Максим Ломакин on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct GiphyTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
